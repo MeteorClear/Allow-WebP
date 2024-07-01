50ec469ed947e2c253e2303dc2db37a4fb052f9f
MeteorClear <103306779+MeteorClear@users.noreply.github.com>
2024-07-01 18:38:24 +0900

    feat: convert webp to png

--- content.js
+++ content.js
@@ -1,4 +1,35 @@
-/* 
- * content.js
- * Not used for now
- */
+document.addEventListener('drop', async (event) => {
+    const items = event.dataTransfer.items;
+
+    for (let i = 0; i < items.length; i++) {
+        if (items[i].kind === 'file' && items[i].type === 'image/webp') {
+            const file = items[i].getAsFile();
+            convertWebPToPNG(file);
+
+            // insert converted file
+        }
+    }
+});
+
+function convertWebPToPNG(file) {
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+        const img = new Image();
+
+        img.src = e.target.result;
+        img.onload = function() {
+            const canvas = document.createElement('canvas');
+            canvas.width = img.width;
+            canvas.height = img.height;
+
+            const ctx = canvas.getContext('2d');
+            ctx.drawImage(img, 0, 0);
+
+            canvas.toBlob(blob => {
+                const pngFile = new File([blob], file.name.replace(/\.webp$/, '.png'), { type: 'image/png' });
+            }, 'image/png');
+        };
+    };
+    reader.readAsDataURL(file);
+}
