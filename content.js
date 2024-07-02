d097dd9e1bf8a7b1f521f0800357b6f00ee67604
MeteorClear <103306779+MeteorClear@users.noreply.github.com>
2024-07-02 16:43:28 +0900

    refactor: separating for convert image to PNG-Blob

--- content.js
+++ content.js
@@ -30,28 +30,40 @@ async function convertWebPToPNG(file, originalEvent) {
     reader.onload = (e) => {
         const img = new Image();
         img.src = e.target.result;
-        img.onload = () => {
-            const canvas = document.createElement('canvas');
-            canvas.width = img.width;
-            canvas.height = img.height;
-            const ctx = canvas.getContext('2d');
-            ctx.drawImage(img, 0, 0);
-            canvas.toBlob(async (blob) => {
-                const pngFile = new File([blob], file.name.replace(/\.webp$/, '.png'), { type: 'image/png' });
+        img.onload = async () => {
+            
+            const pngBlob = await convertImage2PNGBlob(img);
 
-                const dataTransfer = createDataTransfer(pngFile);
+            const pngFile = new File([pngBlob], file.name.replace(/\.webp$/, '.png'), { type: 'image/png' });
 
-                const newEvent = createNewDropEvent(dataTransfer, originalEvent);
+            const dataTransfer = createDataTransfer(pngFile);
 
-                dispatchDropEvent(newEvent, originalEvent.clientX, originalEvent.clientY);
+            const newEvent = createNewDropEvent(dataTransfer, originalEvent);
 
-            }, 'image/png');
+            dispatchDropEvent(newEvent, originalEvent.clientX, originalEvent.clientY);
         };
     };
     reader.readAsDataURL(file);
 }
 
+function convertImage2PNGBlob(image) {
+    return new Promise((resolve) => {
+        console.log("call func : convertImage2PNGBlob :", image);
+
+        const canvas = document.createElement('canvas');
+        canvas.width = image.width;
+        canvas.height = image.height;
+
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(image, 0, 0);
+
+        canvas.toBlob((blob) => resolve(blob), 'image/png');
+    });
+}
+
 function createDataTransfer(file) {
+    console.log("call func : createDataTransfer :", file);
+
     const dataTransfer = new DataTransfer();
     dataTransfer.items.add(file);
 
@@ -74,6 +86,7 @@ function createNewDropEvent(dataTransfer, originalEvent) {
 
     Object.defineProperty(newEvent, 'srcElement', { value: originalEvent.srcElement });
     Object.defineProperty(newEvent, 'target', { value: originalEvent.target });
+    
     newEvent.dataTransfer.dropEffect = originalEvent.dataTransfer.dropEffect;
     newEvent.dataTransfer.effectAllowed = originalEvent.dataTransfer.effectAllowed;
 
